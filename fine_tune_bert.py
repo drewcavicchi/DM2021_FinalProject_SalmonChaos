3f6f3c8d2ade86f178dbb4e55359c52670256d87
alisonhc <alisonhchi@gmail.com>
2021-12-10 15:04:44 +0800

    switching evaluator

--- fine_tune_bert.py
+++ fine_tune_bert.py
@@ -10,11 +10,13 @@ Original file is located at
 #!pip install sentence_transformers
 
 import os
-from sentence_transformers import SentenceTransformer, losses, evaluation, InputExample
+import pandas as pd
+from sentence_transformers import SentenceTransformer, losses, readers, evaluation, InputExample
 from torch.utils.data import DataLoader
 #from google.colab import files
 
 MODEL_NAME = 'bert-base-chinese'
+batch_size = 4
 
 model = SentenceTransformer(MODEL_NAME)
 
@@ -24,29 +26,47 @@ model = SentenceTransformer(MODEL_NAME)
 def get_examples(fpath):
   examples = []
   fname = fpath.split('/')[-1].split('.')[-2]
-  id = 0
+  gu_id = 0
   for line in open(fpath, encoding="utf-8"):
     splits = line.strip().split('\t')
     label = int(splits[2])
     text1 = splits[0]
     text2 = splits[1]
-    guid = "%s-%d" % (fname, id)
-    id += 1
+    guid = "%s-%d" % (fname, gu_id)
+    gu_id += 1
     examples.append(InputExample(guid=guid, texts=[text1, text2], label=label))
   return examples
 
 train_pair_examples = get_examples('all_pairs_balanced.tsv')
 
-train_data_loader = DataLoader(train_pair_examples, batch_size=32, shuffle=True)
+print(train_pair_examples[0])
+
+def get_three_lists_for_evaluator(fpath):
+  texts1 = []
+  texts2 = []
+  labels = []
+  fname = fpath.split('/')[-1].split('.')[-2]
+  for line in open(fpath, encoding="utf-8"):
+    splits = line.strip().split('\t')
+    label = int(splits[2])
+    text1 = splits[0]
+    text2 = splits[1]
+    texts1.append(text1)
+    texts2.append(text2)
+    labels.append(label)
+  return texts1, texts2, labels
+
+train_data_loader = DataLoader(train_pair_examples, batch_size=batch_size, shuffle=True)
 
 train_loss = losses.SoftmaxLoss(model=model,
                                 sentence_embedding_dimension=model.get_sentence_embedding_dimension(),
                                 num_labels=2)
 
-evaluator = evaluation.LabelAccuracyEvaluator(dataloader=train_data_loader, softmax_model=model)
+texts1, texts2, labels = get_three_lists_for_evaluator('all_pairs_balanced.tsv')
+
+evaluator = evaluation.BinaryClassificationEvaluator(batch_size=batch_size, sentences1=texts1, sentences2=texts2, labels=labels)
 
 model_save_path = 'bert_chinese_doc_pair_classifier'
 
 model.fit(train_objectives=[(train_data_loader, train_loss)], epochs=4,
           output_path=model_save_path, evaluator=evaluator, evaluation_steps=500)
-
